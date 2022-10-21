3f1bfe4bebc0df01679b548cb2eea9b97eccb3d3
abedm <abedmohammed353@gmail.com>
2022-10-21 10:39:57 -0400

    Restructured code and layout

--- public/script.js
+++ public/script.js
@@ -9,34 +9,36 @@ const generateCard = function (data) {
   return `
     <article class="card">
       <a class="card__link" href="${data.link}/"></a>
-      <div class="card__figure">
-        <img class="figure__image image-${data.size}" src="${data.link}/final.png" alt="${data.name} image" />
-        <div class="figure__cover">
-          <div class="cover__content">
-            <h3 class="content__name">${data.name}</h3>
-            <div class="content__tags">
+      <img
+        class="card__image image-${data.size}"
+        src="${data.link}/final.png"
+        alt="${data.name} image"
+      />
+      <div class="card__cover">
+        <div class="cover__shadow"></div>
+        <div class="cover__content">
+          <h2 class="content__name">${data.name}</h2>
+          <div class="content__tags">
             ${tags}
-            </div>
           </div>
-          <div class="cover__shadow"></div>
-          <div class="cover__links">
-            <a
-              href="https://github.com/abedmohammed/web-snippets/tree/main/${data.link}/"
-            >
-              <img
-                class="links__github links__image"
-                src="public/images/github-icon.svg"
-                alt="Github icon"
-              />
-            </a>
-            <a href="${data.link}/">
-              <img
-                class="links__live links__image"
-                src="public/images/external-link-icon.svg"
-                alt="Website link icon"
-              />
-            </a>
-          </div>
+        </div>
+        <div class="cover__links">
+          <a
+            href="https://github.com/abedmohammed/web-snippets/tree/main/${data.link}"
+          >
+            <img
+              class="links__github links__image"
+              src="public/images/github-icon.svg"
+              alt="Github icon"
+            />
+          </a>
+          <a href="${data.link}/">
+            <img
+              class="links__live links__image"
+              src="public/images/external-link-icon.svg"
+              alt="Website link icon"
+            />
+          </a>
         </div>
       </div>
     </article>
@@ -55,17 +57,3 @@ const populateCards = function () {
 };
 
 populateCards();
-const cards = Array.from(document.querySelectorAll('.card'));
-
-// cards.forEach((card) => {
-//   card.addEventListener('click', function (e) {
-//     if (e.target.classList.contains('links__image')) return;
-
-//     if (e.shiftKey || e.metaKey) {
-//       window.open(`${window.location.href}/${this.dataset.link}`);
-//       return;
-//     }
-
-//     window.location.href = `/${this.dataset.link}`;
-//   });
-// });

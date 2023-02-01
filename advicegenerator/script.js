8a3414b28e793945790e90b0f3cc9b9f13d85237
abedm <abedmohammed353@gmail.com>
2023-02-01 17:07:30 -0500

    advicegenerator - completed javascript

--- advicegenerator/script.js
+++ advicegenerator/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const generateBtn = document.querySelector('.advice__btn');
+const adviceIDText = document.querySelector('#advice-id');
+const adviceText = document.querySelector('#generated-advice');
+
+const displayAdvice = function (slipObj) {
+  const { id: adviceID, advice: advice } = slipObj;
+  adviceIDText.textContent = adviceID;
+  adviceText.textContent = advice;
+};
+
+const getAdvice = function () {
+  fetch('https://api.adviceslip.com/advice')
+    .then((res) => res.json())
+    .then((res) => displayAdvice(res.slip));
+};
+
+generateBtn.addEventListener('click', function () {
+  getAdvice();
+});
